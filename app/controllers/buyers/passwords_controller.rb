e20aa4e766b53bcf928f473f24640c708043d617
straw15 <yaksoccer@gmail.com>
2022-03-24 17:22:42 +0900

    make it better

--- app/controllers/buyers/passwords_controller.rb
+++ app/controllers/buyers/passwords_controller.rb
@@ -23,12 +23,11 @@ class Buyers::PasswordsController < Devise::PasswordsController
 
   # protected
 
-   def after_resetting_password_path_for(resource)
-      root_path
-   end
+   #def after_resetting_password_path_for(resource)
+    #  root_path
+   #end
 
   # The path used after sending reset password instructions
-    def after_sending_reset_password_instructions_path_for(resource_name)
-      root_path
-    end
+    # root_path
+    #end
 end
--- app/controllers/shops/passwords_controller.rb
+++ app/controllers/shops/passwords_controller.rb
@@ -23,12 +23,12 @@ class Shops::PasswordsController < Devise::PasswordsController
 
   # protected
 
-   def after_resetting_password_path_for(resource)
-    root_path
-   end
+   #def after_resetting_password_path_for(resource)
+    #root_path
+   #end
 
   # The path used after sending reset password instructions
-   def after_sending_reset_password_instructions_path_for(resource_name)
-    root_path
-   end
+   #def after_sending_reset_password_instructions_path_for(resource_name)
+    #root_path
+   #end
 end
