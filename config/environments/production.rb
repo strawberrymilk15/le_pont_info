be1cae42b114e63b5d92ad6ca87207201db11bc2
straw15 <yaksoccer@gmail.com>
2022-03-24 17:53:56 +0900

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "アプリのドメイン名" }
+  config.action_mailer.default_url_options = { host: "https://lepont-info.herokuapp.com/" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
